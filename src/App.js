16eafec6133d051ffaa4125f13f1cd02c7ee372a
edwinchen85 <edwinchen85@gmail.com>
2017-10-22 22:16:54 +0800

    Define, import and render Select component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Output from './Components/Output';
+import Select from './Components/Controls/Select';
 import axios from 'axios';
 
 class App extends Component {
@@ -29,9 +30,21 @@ class App extends Component {
       })
   }
 
+  showHtml(x) {
+    this.setState({ html: x }, this.getSampleText);
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <h1>ReactJS Sample Text Generator</h1>
+        <hr />
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
         <Output value={this.state.text} />
       </div>
     );
