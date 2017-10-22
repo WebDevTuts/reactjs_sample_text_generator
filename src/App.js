57c875d534b28f4bbb9e3ecb08460c495a19989c
edwinchen85 <edwinchen85@gmail.com>
2017-10-22 22:20:51 +0800

    Define, import and render Text component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Output from './Components/Output';
 import Select from './Components/Controls/Select';
+import Text from './Components/Controls/Text';
 import axios from 'axios';
 
 class App extends Component {
@@ -34,12 +35,20 @@ class App extends Component {
     this.setState({ html: x }, this.getSampleText);
   }
 
+  changeParas(number) {
+    this.setState({ paras: number }, this.getSampleText);
+  }
+
   render() {
     return (
       <div className="App container">
         <h1>ReactJS Sample Text Generator</h1>
         <hr />
         <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
           <div className="form-group">
             <label>Include HTML:</label>
             <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
